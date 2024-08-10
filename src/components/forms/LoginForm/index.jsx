c43b83957806ee48187fcf0ededd79c0c1b81ed7
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-08-10 15:42:19 +0300

    Add style validation for Fields

--- src/components/forms/LoginForm/index.jsx
+++ src/components/forms/LoginForm/index.jsx
@@ -1,4 +1,5 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
+import classNames from "classnames";
 import { LOGIN_SCHEMA } from "../../../utils/validationSchemas";
 import styles from "./LoginForm.module.sass";
 
@@ -20,11 +21,25 @@ function LoginForm() {
       validationSchema={LOGIN_SCHEMA}
     >
       {(formikProps) => {
+        const loginClassName = classNames(styles.loginFields, {
+          [styles.valid]:
+            !formikProps.errors.email && formikProps.touched.email,
+          [styles.invalid]:
+            formikProps.errors.email && formikProps.touched.email,
+        });
+
+        const passwordClassName = classNames(styles.loginFields, {
+          [styles.valid]:
+            !formikProps.errors.password && formikProps.touched.password,
+          [styles.invalid]:
+            formikProps.errors.password && formikProps.touched.password,
+        });
+
         return (
           <Form className={styles.loginForm}>
             <div className={styles.inputWrapper}>
               <Field
-                className={styles.loginFields}
+                className={loginClassName}
                 type="email"
                 name="email"
                 placeholder="your_mail@mail.com"
@@ -38,7 +53,7 @@ function LoginForm() {
             </div>
             <div className={styles.inputWrapper}>
               <Field
-                className={styles.loginFields}
+                className={passwordClassName}
                 type="password"
                 name="password"
                 placeholder="Enter a secure password"
